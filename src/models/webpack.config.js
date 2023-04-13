388de1db065736c53c26dddd4e42ed6842ccd08b
williamciva <william.civa@outlook.com>
2023-04-13 15:42:57 -0300

    Atulização do modelo de webpack.config

--- src/models/webpack.config.js
+++ src/models/webpack.config.js
@@ -1,13 +1,16 @@
+var prod;
 var dashboard; process.env.DASHBOARD;
 var baseUrl;
 
-try { dashboard = process.env.DASHBOARD.replace(' ', '');; } catch (error) { dashboard = undefined }
+try { prod = process.env.NODE_ENV.replace(' ', '') === 'production'; } catch (error) { prod = false }
+try { dashboard = process.env.DASHBOARD.replace(' ', ''); } catch (error) { dashboard = undefined }
 try { baseUrl = process.env.PUBLIC_URL.replace(' ', ''); } catch (error) { baseUrl = './' }
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: prod ? 'production' : 'development',
   entry: `./src/dashboards/${dashboard}/${dashboard}.tsx`,
   output: {
     path: `${__dirname}/build/${dashboard}/`,
@@ -36,4 +39,15 @@ module.exports = {
     }),
     new MiniCssExtractPlugin(),
   ],
+  devServer: {
+    client: {
+      progress: true,
+    },
+    open: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
 };
